1402456f08cb0c824b99fdd0e201f6e650e99e13
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-06-13 16:31:42 +0000

    added form for updating in movies show details page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -41,4 +41,27 @@ class MoviesController < ApplicationController
     the_movie.destroy
     redirect_to("/movies")
   end
+
+  def update
+    # get the params of the id
+    the_id = params.fetch("path_id")
+
+    # find the movie records using the id 
+    matching_movies = Movie.where({ :id => the_id })
+    update_movie = matching_movies.at(0)
+
+    # update the informations
+    update_movie.title = params.fetch("the_title")
+    update_movie.year = params.fetch("the_year")
+    update_movie.duration = params.fetch("the_duration")
+    update_movie.description = params.fetch("the_description")
+    update_movie.image = params.fetch("the_image")
+    update_movie.director_id = params.fetch("the_director_id")
+
+    # save the informations
+    update_movie.save
+
+    # redirect back to the page
+    redirect_to("/movies/#{update_movie.id}")
+  end
 end
