892e66b281f3ab876ab7b5d987fed8c5a7af500b
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-06-13 16:18:25 +0000

    added forms to delete in the movies show details page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,7 +31,14 @@ class MoviesController < ApplicationController
     create_new_movie.save
 
     redirect_to("/movies")
+  end
 
-    # 10:17
+  def destroy
+    the_id = params.fetch("path_id")
+
+    matching_movies = Movie.where({ :id => the_id })
+    the_movie = matching_movies.at(0)
+    the_movie.destroy
+    redirect_to("/movies")
   end
 end
