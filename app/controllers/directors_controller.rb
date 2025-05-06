47d22cdbf1e63c02d34287422650fea629d290b0
borvux <bennyjoram@gmail.com>
2025-05-06 18:17:28 -0500

    solution

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,43 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def create
+    new_director = Director.new
+
+    new_director.name = params.fetch("name")
+    new_director.dob = params.fetch("dob")
+    new_director.bio = params.fetch("bio")
+    new_director.image = params.fetch("image")
+
+    new_director.save
+
+    redirect_to("/directors")
+  end
+
+  def destroy
+    director_id = params.fetch("path_id")
+
+    matching_directors = Director.where({ :id => director_id  })
+    the_director = matching_directors.first 
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+
+  def update
+    director_id = params.fetch("path_id")
+
+    matching_directors = Director.where({ :id => director_id })
+    the_director = matching_directors.first
+
+    the_director.name = params.fetch("name")
+    the_director.dob = params.fetch("dob")
+    the_director.bio = params.fetch("bio")
+    the_director.image = params.fetch("image")
+
+    the_director.save
+
+    redirect_to("/directors/#{the_director.id}")
+  end
 end
